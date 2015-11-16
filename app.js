564dbf4101fb4c2dad6e727889c5afb9f2448b7c
Kate Wood <katewood611@gmail.com>
2015-11-16 10:17:35 -0800

    adds root route

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ var express = require('express'),
 	ejs		= require('ejs')
 	port 	= process.env.PORT || 3000
 
+// app configurations
 
+
+// middleware
+
+
+// basic routes
+app.get('/', function(req, res) {
+	// response
+	res.json({message: 'It works'})
+})
 
 app.listen(port, function() {
 	console.log('Server running on', port)
